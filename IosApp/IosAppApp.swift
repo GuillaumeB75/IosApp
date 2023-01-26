a321bb3ee9fd5534838472e3e131ca4f9c67119c
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-01-26 19:31:59 +0100

    Initial Commit

---
+++ IosApp/IosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosAppApp.swift
+//  IosApp
+//
+//  Created by Guillaume Bézie on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct IosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
