56e2c1a66fa6f6e4900ea2b595391ad737097f78
GuillaumeB75 <guillaume.bezie@yahoo.com>
2023-02-04 19:34:55 +0100

    add text for test

--- IosApp/ContentView.swift
+++ IosApp/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Salut, le Monde!")
             Text("Welcome everyone")
+            
                 
         }
         .padding()
